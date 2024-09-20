4fc9c182353476c60c71e5c201094fd804993ecc
hienvv17 <hien.vo@xpodc.com>
2024-09-20 16:06:54 +0700

    simplify

--- src/app/page.js
+++ src/app/page.js
@@ -9,33 +9,28 @@ const initialRestaurants = [];
 emailjs.init(process.env.PUBLIC_SERVICE_KEY);
 
 export default function RandomWheel() {
+  const API_KEY = process.env.GOONG_MAP_API_KEY;
   const canvasRef = useRef(null);
   const [restaurants, setRestaurants] = useState([]);
   const [chosenRestaurant, setChosenRestaurant] = useState("");
   const [isSpinning, setIsSpinning] = useState(false);
   const [showResult, setShowResult] = useState(false);
-  const API_KEY = process.env.GOONG_MAP_API_KEY;
   const [listSuggestLocation, setListSuggestLocation] = useState();
   const [keyword, setKeyword] = useState("");
-  const [emails, setEmails] = useState(""); // State for email addresses
-  const [emailMessage, setEmailMessage] = useState(""); // State for email message
-  const [isSearchActive, setIsSearchActive] = useState('result');
-  const [mapsLink, setMapsLink] = useState('');
-
-
-  const handleChange = (event) => {
-    setEmails(event.target.value);
-    localStorage.setItem("emails", JSON.stringify(emails))
-  };
+  const [isSearchActive, setIsSearchActive] = useState('add');
+  const [name, setName] = useState('');
 
   const debounce = (func, wait) => {
     let timeout;
-
     return (...args) => {
       clearTimeout(timeout);
       timeout = setTimeout(() => func(...args), wait);
     };
   };
+
+  const mealTime = () => {
+
+  }
 
   function randomHexColor() {
     return (
@@ -60,17 +55,14 @@ export default function RandomWheel() {
     const arc = (Math.PI * 2) / restaurants?.length;
     const spinAngle = Math.random() * Math.PI * 2;
     const totalRotation = spins * Math.PI * 2 + spinAngle;
-
     canvas.style.transform = `rotate(${totalRotation - Math.PI / 2}rad)`;
-
     const finalAngle = totalRotation % (Math.PI * 2);
     const selectedIndex = Math.floor(
       (restaurants?.length - finalAngle / arc) % restaurants?.length
     );
 
     setTimeout(() => {
-      setChosenRestaurant(`${restaurants[selectedIndex].name} tại ${restaurants[selectedIndex].address}`);
-      setMapsLink(`https://www.google.com/maps?q=${restaurants[selectedIndex].lat},${restaurants[selectedIndex].lng}`);
+      setChosenRestaurant(`${restaurants[selectedIndex].name}`);
       setShowResult(true);
       setIsSpinning(false);
     }, 5000);
@@ -91,6 +83,16 @@ export default function RandomWheel() {
     setRestaurants(updatedRestaurants);
     localStorage.setItem("restaurants", JSON.stringify(updatedRestaurants));
   };
+  const addRestaurantName = (e) => {
+    e.preventDefault();
+    const nameList = name.split('\n').map(item => item.trim()).filter(item => item);
+    if (!nameList.length) return;
+    const newItems = nameList.map(item => ({ name: item, address: '', lat: '', lng: '' }));
+    const updatedRestaurants = [...restaurants, ...newItems];
+    setRestaurants(updatedRestaurants);
+    localStorage.setItem("restaurants", JSON.stringify(updatedRestaurants));
+    setName('');
+  }
 
   const deleteRestaurant = (index) => {
     const updatedRestaurants = restaurants.filter((_, i) => i !== index);
@@ -106,32 +108,13 @@ export default function RandomWheel() {
       setListSuggestLocation(data.data.predictions);
     }
   };
-  const sendGroupEmail = (e) => {
-    e.preventDefault();
-    const emailArray = emails.split('\n').filter(Boolean).map((email) => email.trim());
-    if (emailArray.length === 0) {
-      setShowResult(false);
-      return;
-    }
-    const templateParams = {
-      to_email: emailArray,
-      message: `Hi guy, we are going to have meal togeter!! \n Here is the link google map for our restaurant:\n ${mapsLink} `,
-    };
-
-    emailjs.send(process.env.SERVICE_ID, process.env.TEMPLATE_ID, templateParams)
-      .then((response) => {
-        alert('Emails sent successfully!', response.status);
-      }, (error) => {
-        alert('Error sending emails: ', error);
-      });
-    setShowResult(false);
-  };
-
   useEffect(() => {
     const savedRestaurants = localStorage.getItem("restaurants");
-
     if (savedRestaurants) {
-      setRestaurants(JSON.parse(savedRestaurants));
+      const restaurants = JSON.parse(savedRestaurants);
+      console.log(restaurants);
+      setRestaurants(restaurants);
+      setIsSearchActive(restaurants.length > 0 ? 'result' : 'add');
     } else {
       setRestaurants(initialRestaurants);
       localStorage.setItem("restaurants", JSON.stringify(initialRestaurants));
@@ -197,144 +180,150 @@ export default function RandomWheel() {
 
 
   return (
-    <div
-      className={`${styles.body} px-4 md:px-16 lg:px-24 max-xl:flex-col-reverse`}
-    >
-      <div className="min-h-[400px] max-h-[400px] min">
-        {/* Tab Navigation */}
-        <div className="tabs text-white flex p-4 gap-6">
-          <button
-            className={`tab-button px-4 py-2 ${isSearchActive == 'result' ? "bg-blue-500" : ""} rounded-md`}
-            onClick={() => { setIsSearchActive('result'); setListSuggestLocation([]) }}
-          >
-            Restaurants list
-          </button>
-          <button
-            className={`tab-button px-4 py-2 ${isSearchActive == 'search' ? "bg-blue-500" : ""} rounded-md`}
-            onClick={() => setIsSearchActive('search')}
-          >
-            Search
-          </button>
-          <button
-            className={`tab-button px-4 py-2 ${isSearchActive == 'email' ? "bg-blue-500" : ""} rounded-md`}
-            onClick={() => setIsSearchActive('email')}
-          >
-            Email list
-          </button>
-        </div>
-
-        {/* Search Section */}
-        {isSearchActive == 'search' && (
-          <div className="form-add-restaurant mt-4 h-full">
-            <input
-              placeholder="Search restaurant..."
-              onChange={(e) => {
-                setKeyword(e.target.value);
-                debounce(() => {
-                  fetchSuggesstLocation(e.target.value);
-                }, 1000)();
-              }}
-              className="border p-2 w-full"
-            />
-            {listSuggestLocation?.length > 0 && keyword.length > 0 && (
-              <div className="list-locations">
-                {listSuggestLocation.map((item, idx) => (
-                  <p
-                    key={idx}
-                    onClick={addRestaurant(item?.structured_formatting?.main_text)}
-                    className="cursor-pointer p-2 hover:bg-gray-200"
-                  >
-                    {item.description}
-                  </p>
-                ))}
-              </div>
-            )}
-          </div>
-
-        )}
-
-        {/* List Section */}
-        {isSearchActive == 'result' && restaurants.length > 0 && (
-          <div className="restaurant-list mt-4">
-
-            <p className="text-lg font-semibold border-b border-gray-400 p-4">
-              List of Restaurants
-            </p>
-
-            <div className="grid max-h-[50vh] overflow-auto">
-              {restaurants.map((item, index) => (
-                <div
-                  key={index}
-                  className="flex items-center justify-between gap-6 p-4 border-b"
-                >
-                  <p className="text-sm text-gray-900">{item.name}</p>
-                  <button
-                    className="px-4 py-2 text-white bg-red-400 rounded-md"
-                    onClick={() => deleteRestaurant(index)}
-                  >
-                    Delete
-                  </button>
-                </div>
-              ))}
-            </div>
-          </div>
-        )}
-        {/* Email Section */}
-        {isSearchActive == 'email' && (
-          <div>
-            <textarea
-              value={emails}
-              className="border rounded-md w-full p-3 focus-visible:outline-none"
-              onChange={handleChange}
-              placeholder="Enter your team email, separated by enter"
-              rows="5"
-              cols="50"
-            />
-          </div>
-        )}
-      </div>
-
-      <div className={`${styles.wheelContainer} shrink-0`}>
-        <div className={styles.selector}></div>
-        <canvas ref={canvasRef} className={styles.wheel}></canvas>
-        <button
-          className={styles.wheelCenter}
-          onClick={spinWheel}
-          aria-label="Spin the wheel"
-        >
-          SPIN
-        </button>
+    <div className="bg-[#baf2f5]">
+      <div className="items-center justify-between text-center p-2">
+        <h1 className="font-bold text-3xl text-transparent bg-clip-text bg-gradient-to-r to-red-600 from-sky-400">
+          Foodie Roulette And Fun
+        </h1>
       </div>
       <div
-        className={`${styles.resultMessage} ${showResult ? styles.show : ""}`}
-        role="alert"
-        aria-live="polite"
+        className={`${styles.body} px-4 md:px-16 lg:px-24 max-xl:flex-col-reverse`}
       >
-        <h1>
-          <span className={styles.resultText}>Let's have</span>
-          <br />
-          <span className={styles.restaurantName}>{chosenRestaurant}</span>
-          <br />
-          <span className={styles.resultText}>tonight!</span>
-        </h1>
-        <div className="result-buttons mt-4">
+        <div className="min-h-[400px] max-h-[400px] w-[343px] md:w-[500px] text-xs md:text-sm lg:text-lg">
+          {/* Tab Navigation */}
+          <div className="tabs text-black flex p-4 gap-6">
+            <button
+              className={`tab-button px-4 py-2 ${isSearchActive == 'result' ? "bg-blue-500 text-white" : ""} rounded-md`}
+              onClick={() => { setIsSearchActive('result'); setListSuggestLocation([]) }}
+            >
+              Restaurants list
+            </button>
+            <button
+              className={`tab-button px-4 py-2 ${isSearchActive == 'search' ? "bg-blue-500 text-white" : ""} rounded-md`}
+              onClick={() => setIsSearchActive('search')}
+            >
+              Search Online
+            </button>
+            <button
+              className={`tab-button px-4 py-2 ${isSearchActive == 'add' ? "bg-blue-500 text-white" : ""} rounded-md`}
+              onClick={() => setIsSearchActive('add')}
+            >
+              Add name
+            </button>
+          </div>
+
+          {/* Search Section */}
+          {isSearchActive == 'search' && (
+            <div className="form-add-restaurant mt-4 h-full items-center justify-center">
+              <input
+                placeholder="Search restaurant..."
+                onChange={(e) => {
+                  setKeyword(e.target.value);
+                  debounce(() => {
+                    fetchSuggesstLocation(e.target.value);
+                  }, 3000)();
+                }}
+                className="border p-2 w-full"
+              />
+              {listSuggestLocation?.length > 0 && keyword.length > 0 && (
+                <div className="list-locations">
+                  {listSuggestLocation.map((item, idx) => (
+                    <p
+                      key={idx}
+                      onClick={addRestaurant(item?.structured_formatting?.main_text)}
+                      className="cursor-pointer p-2 hover:bg-gray-200"
+                    >
+                      {item.description}
+                    </p>
+                  ))}
+                </div>
+              )}
+            </div>
+
+          )}
+
+          {/* List Section */}
+          {isSearchActive == 'result' && restaurants.length > 0 && (
+            <div className="restaurant-list mt-4">
+
+              <p className="text-lg font-semibold border-b border-gray-400 p-4">
+                List of Restaurants
+              </p>
+
+              <div className="grid max-h-[50vh] overflow-auto">
+                {restaurants.map((item, index) => (
+                  <div
+                    key={index}
+                    className="flex items-center justify-between gap-6 p-4 border-b"
+                  >
+                    <p className="text-sm text-gray-900">{item.name}</p>
+                    <button
+                      className="px-4 py-2 text-white bg-red-400 rounded-md"
+                      onClick={() => deleteRestaurant(index)}
+                    >
+                      Delete
+                    </button>
+                  </div>
+                ))}
+              </div>
+            </div>
+          )}
+          {/* Add Name Section */}
+          {isSearchActive == 'add' && (
+            <div className="items-center justify-center gap-4 flex">
+              <textarea
+                className="border rounded-md w-3/5 p-3 focus-visible:outline-none"
+                onChange={(e) => setName(e.target.value)}
+                placeholder="Enter names here..."
+                rows="5"
+                cols="50"
+              />
+              <button
+                className="border p-2 w-1/5 bg-blue-500 text-white rounded-md"
+                onClick={addRestaurantName}
+              >
+                Add
+              </button>
+            </div>
+          )}
+        </div>
+
+        <div className={`${styles.wheelContainer} shrink-0`}>
+          <div className={styles.selector}></div>
+          <canvas ref={canvasRef} className={styles.wheel}></canvas>
           <button
-            className="px-4 py-2 bg-red-300 text-white rounded-md"
-            onClick={() => {
-              setShowResult(false);
-            }}
+            className={styles.wheelCenter}
+            onClick={spinWheel}
+            aria-label="Spin the wheel"
           >
-            Close
-          </button>
-          <button
-            className="px-4 py-2 bg-green-500 text-white rounded-md ml-4"
-            onClick={sendGroupEmail}
-          >
-            Let's go
+            SPIN
           </button>
         </div>
-      </div>
+        <div
+          className={`${styles.resultMessage} ${showResult ? styles.show : ""}`}
+          role="alert"
+          aria-live="polite"
+        >
+          <h1>
+            <span className={styles.resultText}>Let's have</span>
+            <br />
+            <span className={styles.restaurantName}>{chosenRestaurant}</span>
+            <br />
+            <span className={styles.resultText}>tonight!</span>
+          </h1>
+          <div className="result-buttons mt-4">
+            <button
+              className="px-4 py-2 bg-red-300 text-white rounded-md"
+              onClick={() => {
+                setShowResult(false);
+              }}
+            >
+              Close
+            </button>
+          </div>
+        </div>
 
-    </div >
+      </div >
+    </div>
   );
 }
